4a9a9f357c5517692a9968af9194067f7cb25d95
Joe Nguyen <joe.nguyen@toyota.com>
2016-09-06 08:59:08 -0500

    updates from latest chefdk

--- recipes/repo.rb
+++ recipes/repo.rb
@@ -8,19 +8,36 @@ directory repo_dir
 template "#{repo_dir}/LICENSE" do
   source "LICENSE.#{context.license}.erb"
   helpers(ChefDK::Generator::TemplateHelper)
+  action :create_if_missing
+end
+
+cookbook_file "#{repo_dir}/.chef-repo.txt" do
+  source "repo/dot-chef-repo.txt"
+  action :create_if_missing
 end
 
 cookbook_file "#{repo_dir}/README.md" do
   source "repo/README.md"
+  action :create_if_missing
 end
 
 cookbook_file "#{repo_dir}/chefignore" do
   source "chefignore"
+  action :create_if_missing
 end
 
-%w{cookbooks data_bags environments roles}.each do |tlo|
+directories_to_create = %w{ cookbooks data_bags }
+
+if context.use_roles
+  directories_to_create += %w{ roles environments }
+else
+  directories_to_create += %w{ policies }
+end
+
+directories_to_create.each do |tlo|
   remote_directory "#{repo_dir}/#{tlo}" do
     source "repo/#{tlo}"
+    action :create_if_missing
   end
 end
 
@@ -30,23 +47,21 @@ cookbook_file "#{repo_dir}/cookbooks/README.md" do
   else
     source "cookbook_readmes/README.md"
   end
+  action :create_if_missing
 end
 
 # git
-execute("initialize-git") do
-  command("git init .")
-  cwd repo_dir
-end
-
-template "#{repo_dir}/.gitignore" do
-  source "repo/gitignore.erb"
-  helpers(ChefDK::Generator::TemplateHelper)
-end
-
-# create .chef directory
-directory "#{repo_dir}/.chef"
-
-template "#{repo_dir}/.chef/knife.rb" do
-  source "knife.rb.erb"
-  helpers(ChefDK::Generator::TemplateHelper)
+if context.have_git
+  if !context.skip_git_init
+    execute("initialize-git") do
+      command("git init .")
+      cwd repo_dir
+      not_if { File.exist?("#{repo_dir}/.gitignore") }
+    end
+  end
+  template "#{repo_dir}/.gitignore" do
+    source "repo/gitignore.erb"
+    helpers(ChefDK::Generator::TemplateHelper)
+    action :create_if_missing
+  end
 end
